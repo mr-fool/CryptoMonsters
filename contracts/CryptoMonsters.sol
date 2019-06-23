237ff02ef13db6f2d90230613c1d6065db1131a8
mr-fool <mr-fool@users.noreply.github.com>
2019-06-22 23:33:08 -0600

    initial commit

---
+++ contracts/CryptoMonsters.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.5.0;
+
+import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol';
+import 'openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol';
+
+contract CryptoMonsters is ERC721Full, ERC721Mintable {
+
+    struct Monster {
+        string name;
+        uint level;
+        uint attackPower;
+        uint defensePower;
+    }
+
+}
