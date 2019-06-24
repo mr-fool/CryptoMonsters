68897054481a2dc37e7d35570d4624d53dbbc394
mr-fool <mr-fool@users.noreply.github.com>
2019-06-23 22:51:47 -0600

    added modifier

--- contracts/CryptoMonsters.sol
+++ contracts/CryptoMonsters.sol
@@ -18,6 +18,14 @@ contract CryptoMonsters is ERC721Full, ERC721Mintable {
         //Only the owner can create crypto monster
         owner = msg.sender;
     }
+    /**
+   * @dev Guarantees msg.sender is owner of the given token
+   * @param _tokenId uint256 ID of the token to validate its ownership belongs to msg.sender
+   */
+    modifier onlyOwnerOf(uint256 _tokenId) {
+        require(ownerOf(_tokenId) == msg.sender);
+        _;
+    }
 
     function createMonster(string memory _name, uint _level, uint _attackPower,uint _defensePower, address _to) public {
         require(owner == msg.sender);
@@ -26,4 +34,7 @@ contract CryptoMonsters is ERC721Full, ERC721Mintable {
         _mint(_to, id);
     }
 
+    function battle(uint _monsterId, uint _targetId) onlyOwnerOf(_monsterId) public {
+
+    }
 }
