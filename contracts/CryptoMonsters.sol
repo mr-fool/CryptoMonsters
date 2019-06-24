42db516d011399e4351d40b5d743e1ddd39c87d9
mr-fool <mr-fool@users.noreply.github.com>
2019-06-23 19:10:41 -0600

    createMonster

--- contracts/CryptoMonsters.sol
+++ contracts/CryptoMonsters.sol
@@ -19,11 +19,11 @@ contract CryptoMonsters is ERC721Full, ERC721Mintable {
         owner = msg.sender;
     }
 
-    function createMonster(string memory _name, address _to) public {
+    function createMonster(string memory _name, uint _level, uint _attackPower,uint _defensePower, address _to) public {
         require(owner == msg.sender);
-        uint id =  monsters.lengh;
-        monsters.push(Monster(_name, 1, 100, 100));
-        _mint();
+        uint id =  monsters.length;
+        monsters.push(Monster(_name, _level, _attackPower, _defensePower));
+        _mint(_to, id);
     }
 
 }
