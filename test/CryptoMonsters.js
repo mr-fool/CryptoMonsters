183058190d60e3b75869442a44eb826c4a8c05d1
mr-fool <mr-fool@users.noreply.github.com>
2019-06-25 17:41:17 -0600

    logic issue

--- test/CryptoMonsters.js
+++ test/CryptoMonsters.js
@@ -11,6 +11,7 @@ contract("CryptoMonsters", async accounts  => {
     
     describe('createMonster', () => {
         it("Only sender can create monsters", async () => {
+            let createMonsters = await instance.createMonster("testMonster",1,1,1,accounts[1]);
             let owner = await instance.owner();
             assert.equal(owner, accounts[0], "owner is the msg.sender");
         });
