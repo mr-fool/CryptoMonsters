c955eaa371c903cfcecdaeacaf2a259c9a78c68f
mr-fool <mr-fool@users.noreply.github.com>
2019-06-24 21:35:48 -0600

    cyka

--- test/CryptoMonsters.js
+++ test/CryptoMonsters.js
@@ -3,14 +3,15 @@ var CryptoMonsters = artifacts.require("CryptoMonsters");
 let instance;
 
 beforeEach( async () => {
-  instance = await CryptoMonsters.deployed();
-
-});
+    instance = await CryptoMonsters.deployed();
+  
+  });
 
 contract("CryptoMonsters", async accounts  => {
+    
     describe('createMonster', () => {
         it("Only sender can create monsters", async () => {
-            //let owner = await instance.owner();
+            let owner = await instance.owner();
             //assert.equal(owner, accounts[0], "owner is the msg.sender");
         });
     });
