440a85fb4698eaf8de28f49153f306ec735e2d44
mr-fool <mr-fool@users.noreply.github.com>
2019-06-25 22:52:13 -0600

    Update CryptoMonsters.js

--- test/CryptoMonsters.js
+++ test/CryptoMonsters.js
@@ -11,9 +11,19 @@ contract("CryptoMonsters", async accounts  => {
     
     describe('createMonster', () => {
         it("Only sender can create monsters", async () => {
-            let createMonsters = await instance.createMonster("testMonster",1,1,1,accounts[1]);
+            
             let owner = await instance.owner();
             assert.equal(owner, accounts[0], "owner is the msg.sender");
+        });
+
+        it("Check if the attackPower is set correctly", async() => {
+            let createMonster;
+            try{
+                createMonster = await instance.createMonster("testMonster",0,1,1,accounts[1]);
+            }
+            catch(error){
+                assert.ok(error.message.indexOf('revert')>=0, 'attackPower check works');
+            }
         });
     });
 
